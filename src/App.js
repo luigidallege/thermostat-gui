9d89e4265b55f316ee101d7543fdd8b4e29b722f
Jordan Pohlman <jordan-pohlman@uiowa.edu>
2018-11-28 17:01:07 -0600

    add styling for the landing page and add html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="screen">
+        <div id="toolbar">
+          <div id="date-time">
+            <h1>4:47 PM</h1>
+            <h1 id="date">Wed, Nov. 7th</h1>
+          </div>
+          <div id="nav">
+            <button>Cal</button>
+            <button>S</button>
+          </div>
+        </div>
+          
+        <div id="main">
+          <div id="hvac-control">
+            <button id="hvac-button">Auto</button>
+            <button id="hvac-button">Heat</button>
+            <button id="hvac-button">AC</button>
+          </div>
+          <div id="temp-container">
+            <p id="curr-temp">30&#730;</p>
+            <div id="status-container">
+              <p id="hvac-status-id">Status: </p>
+              <p id="hvac-status">Heating On</p>
+            </div>
+          </div>
+          <div id="adjust-container">
+            <div id="adjust-box">
+              <div id="arrow-up"/>
+              <p id="desired-temp-label">SET TEMP</p>
+              <p id="desired-temp">50&#730;</p>
+              <div id="arrow-down"/>
+              <button id="hold-btn">HOLD</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
