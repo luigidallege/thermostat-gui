bf78f230767e45dbe8edca04d2ca5d903fd47443
Jordan Pohlman <jordanpohlman@gmail.com>
2018-11-29 20:27:26 -0600

    Update App.js
    add routing to app.js and take away main page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar'
+import Scheduler from './Scheduler'
+import EditSetPointTime from './EditSetPointTime'
+import EditSetPointTemp from './EditSetPointTemp'
+// Import routing components
+import {
+    Switch,
+    Route
+} from 'react-router-dom'
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div id="screen">
-        <NavBar></NavBar>
+          <NavBar></NavBar>
 
-        <div id="main">
-          <div id="hvac-control">
-            <button id="hvac-button">Auto</button>
-            <button id="hvac-button">Heat</button>
-            <button id="hvac-button">AC</button>
-          </div>
-          <div id="temp-container">
-            <p id="curr-temp">30&#730;</p>
-            <div id="status-container">
-              <p id="hvac-status-id">Status: </p>
-              <p id="hvac-status">Heating On</p>
-            </div>
-          </div>
-          <div id="adjust-container">
-            <div id="adjust-box">
-              <div id="arrow-up"/>
-              <p id="desired-temp-label">SET TEMP</p>
-              <p id="desired-temp">50&#730;</p>
-              <div id="arrow-down"/>
-              <button id="hold-btn">HOLD</button>
-            </div>
-          </div>
-        </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/setpoints" component={Scheduler} />
+            <Route path="/editsetpointtime" component={EditSetPointTime} />
+            <Route path="/editsetpointtemp" component={EditSetPointTemp} />
+          </Switch>
       </div>
-    );
+    );    
   }
 }
 
