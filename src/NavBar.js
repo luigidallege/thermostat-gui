f25e6b70a7c0f135366fc260fe8db466a45f8fd9
Nick Dallege <nick-dallege@uiowa.edu>
2018-11-29 22:08:33 -0600

    Conditional Render and title change

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { DateRange, Settings } from '@material-ui/icons'
+import { DateRange, Home, Settings } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
 
+function NavIcon() {
+
+  if (window.location.href === "http://localhost:3001/") {
+    return (
+      <Link to="/setpoints">
+        <DateRange id="nav-btn" fontSize="large" />
+      </Link>
+    )
+  } else {
+    return (
+      <Link to="/">
+        <Home id="nav-btn" fontSize="large" />
+      </Link>
+    )
+  }
+}
+
 class NavBar extends Component {
-  render(){
-    return(
+  render() {
+
+    let navIcon = NavIcon();
+
+    return (
       <AppBar position="static">
         <Toolbar id="toolbar-homepage">
           <div id="date-time">
@@ -13,10 +33,8 @@ class NavBar extends Component {
             <Typography id="date">Wed, Nov. 7th</Typography>
           </div>
           <div id="nav">
-            <Link to="/setpoints">
-              <DateRange id="nav-btn" fontSize="large"/>
-            </Link>
-            <Settings id="nav-btn" fontSize="large"/>
+            {navIcon}
+            <Settings id="nav-btn" fontSize="large" />
           </div>
         </Toolbar>
       </AppBar>
