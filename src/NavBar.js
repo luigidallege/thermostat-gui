5032f3a7aef836eec3c0bc1c47e9552e6d6c64ab
Nick Dallege <nick-dallege@uiowa.edu>
2018-11-29 23:28:48 -0600

    fixed time issue

--- src/NavBar.js
+++ src/NavBar.js
@@ -30,19 +30,27 @@ function HiddenNavBar() {
   }
 }
 
+function DateTime() {
+  let time = moment();
+  return(
+  <div>
+    <Typography id="time" align="left">{time.format("h:mm a")}</Typography>
+    <Typography id="date" align="left">{time.format("ddd, MMM Do")}</Typography>
+  </div>
+  );
+}
+
 class NavBar extends Component {
   render() {
     let isHidden = HiddenNavBar()
     let navIcon = NavIcon();
-    let time = moment();
-
-    console.log(isHidden)
+    let dateTime = DateTime();
 
     return (
       <AppBar position="static">
         <Toolbar id={isHidden ? 'toolbar' : 'toolbar-homepage'}>
           <div id="date-time" isHidden={isHidden}>
-            {isHidden ? null : <DateTime/>}
+            {isHidden ? null : dateTime}
           </div>
           <div id="nav">
             {navIcon}
@@ -56,12 +64,5 @@ class NavBar extends Component {
   }
 
 }
-
-const DateTime = () => (
-  <div>
-    <Typography id="time">4:47 PM</Typography>
-    <Typography id="date">Wed, Nov. 7th</Typography>
-  </div>
-)
 
 export default NavBar
