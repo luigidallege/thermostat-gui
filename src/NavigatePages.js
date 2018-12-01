2a2bca7bb854ee873254751fbb9378cfea59325f
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 10:57:55 -0600

    Update NavigatePages.js
    change Button to button

--- src/NavigatePages.js
+++ src/NavigatePages.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { button } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 export class NavigatePages extends Component{  
     render() {   
       return(
         <div id="button-container">
-          <Button class="button back-btn" onClick={this.props.goBack}>BACK</Button>
-          <Button class="button next-btn">
-            <Link to={this.props.nextPage} >NEXT</Link>
-          </Button>
+          <button class="button back-btn" onClick={this.props.goBack}>BACK</button>
+          <a href={this.props.nextPage} class="button next-btn">NEXT</a>
         </div>
       );
     }
