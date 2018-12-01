2f73bc3a9bc210e6ea5a0c10af399e86561f01ea
Jordan Pohlman <jordanpohlman@gmail.com>
2018-12-01 10:58:24 -0600

    Update Home.js
    first attempt at GPIO

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var HeatLED = new Gpio(21, 'out'); //use GPIO pin 4, and specify that it is output
+// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+// var HeatLED = new Gpio(21, 'out'); //use GPIO pin 21, and specify that it is output
 
 class Home extends Component {
   constructor(props){
@@ -10,21 +10,21 @@ class Home extends Component {
     this.state = {
       heatStatus: 0
     };
-    this.setHeat = this.setHeat.bind(this);
+    // this.setHeat = this.setHeat.bind(this);
   }
 
-  setHeat() { 
-    this.setState({heatStatus: 1});
-    HeatLED.writeSync(this.state.heatStatus); //turn LED on or off depending on the button state (0 or 1)
-  }
+  // setHeat() { 
+  //   this.setState({heatStatus: 1});
+  //   HeatLED.writeSync(this.state.heatStatus); //turn LED on or off depending on the button state (0 or 1)
+  // }
 
   render() {
     return (
       <div id="main-homepage">
         <div id="hvac-control">
-          <button id="hvac-button">Auto</button>
-          <button id="hvac-button" onClick={this.setHeat}>Heat</button>
-          <button id="hvac-button">AC</button>
+          <button class="button hvac-button">Auto</button>
+          <button class="button hvac-button" >Heat</button>
+          <button class="button hvac-button">AC</button>
         </div>
         <div id="temp-container">
           <p id="curr-temp">30&#730;</p>
@@ -39,7 +39,7 @@ class Home extends Component {
             <p id="desired-temp-label">SET TEMP</p>
             <p id="desired-temp">50&#730;</p>
             <div id="arrow-down"/>
-            <button id="hold-btn">HOLD</button>
+            <button class="button hold-btn">HOLD</button>
           </div>
         </div>
       </div>
